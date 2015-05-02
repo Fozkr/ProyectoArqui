42b6630da096e9ec974bd984b83c1f6f28d3a053
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-05-01 22:44:54 -0600

    Proyecto creado, primer commit.

---
+++ ProyectoArqui/ProyectoArqui/Procesador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArqui
+{
+    /*
+     * 
+     */
+    class Procesador
+    {
+        //Atributos
+        short programCounter;
+        int* registros;
+        int** cacheDatos;
+        int** memoriaPrincipal;
+        //sem semaforoCache;
+        //int** directorio;
+        //sem semaforoDirectorio
+        Boolean atrasado;
+    }
+}
