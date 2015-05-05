7bd0fe25668a4f0291c09ed2e5d35d51d0d44fb8
María Taborda Arroyo <mtaborda37@gmail.com>
2015-05-05 16:16:58 -0600

    Simulación básica terminada con Denise
    Implementación de todo lo relacionado con el procesador y las caches.

---
+++ ProyectoArqui/simulador/Bloque.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArqui.simulador
+{
+    class Bloque
+    {
+        private int[] palabras = new int[4];
+
+        Bloque()
+        {
+            for (int i = 0; i < palabras.Length; ++i)
+            {
+                palabras[i] = 0;
+            }
+        }
+
+        public int get(int index)
+        {
+            return palabras[index];
+        }
+
+        public void set(int index, int element)
+        {
+            palabras[index] = element;
+        }
+
+    }
+}
---
+++ ProyectoArqui/simulador/Instrucciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArqui.simulador
+{
+    class CacheInstrucciones
+    {
+
+        private Instruccion[] instrucciones;
+
+        public CacheInstrucciones() 
+        {
+            //Obtener instrucciones de los archivos
+        }
+
+        public Instruccion obtenerInstruccion(int dirMem)
+        {
+            return instrucciones[dirMem / 4];
+        }
+
+    }
+}
